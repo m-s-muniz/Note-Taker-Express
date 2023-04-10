cd322791327443bc33d0407719c45473a1ddc7ef
Michael Muniz <m.s.muniz@sbcglobal.net>
2023-04-10 17:52:05 -0500

    Finialized setup of server.js.

--- server.js
+++ server.js
@@ -1,22 +1,21 @@
+// Import Express.js
 const express = require('express');
+// Import our modular routers for /apiRoutes and /htmlRoutes
 const apiRoutes = require('./routes/apiRoutes.js');
 const htmlRoutes = require('./routes/htmlRoutes.js');
-
+// The Ports the Express.js server will run public and server side
 const PORT = process.env.PORT || 3001;
-
+// Initialize an instance of Express.js
 const app = express();
+// Middleware for parsing JSON and urlencoded form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/api',apiRoutes);
+app.use('/',htmlRoutes);
+app.use(express.static('public'));
 
 
-
-
-
-
-
-
-
-
-
-
+// listen() method is responsible for listening for incoming connections on the specified por
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
 );
