65862a335ced02dc5ee17bcd2228b6abc61630d3
Michael Muniz <m.s.muniz@sbcglobal.net>
2023-04-12 17:45:59 -0500

    Added the ablility to delete specific note in the Delete Route handler.

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const { v4: uuidv4 } = require('uuid');
 const fs = require ("fs");
+
 // GET Route for retrieving all the notes from db.json file
 router.get("/notes", (req, res) => {
     const data = fs.readFileSync("db/db.json");
@@ -20,10 +21,17 @@ router.post("/notes",(req, res) => {
 
 });
 
+// DELETE Route for a specific note
 
-
-
-
+router.delete('/notes/:id', (req, res) => {
+    const notesJson = JSON.parse(fs.readFileSync("db/db.json","utf8"));
+    const updatedNotes = notesJson.filter((delNote) => delNote.id !== req.params.id);
+    fs.writeFileSync("db/db.json", JSON.stringify(updatedNotes));
+   
+        // Respond to the DELETE request
+    res.json("Note has been deleted.");
+ 
+ });
 
 
 
--- server.js
+++ server.js
@@ -12,8 +12,8 @@ const app = express();
 // Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/api',apiRoutes);
-app.use('/',htmlRoutes);
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
 app.use(express.static('public'));
 
 
