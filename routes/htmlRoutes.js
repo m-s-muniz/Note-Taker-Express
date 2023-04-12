8eb1d896cbb5aba1ccf8a00d6c1dd62a824f3906
Michael Muniz <m.s.muniz@sbcglobal.net>
2023-04-12 04:19:45 -0500

    Added the npm uuid to aid with giving each note a unique id.

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,10 +1,11 @@
 const router = require('express').Router();
 const path = require('path');
 
+// Routes to the notes.html webpage
 router.get("/notes", (req,res) => {
     res.sendFile(path.join(__dirname, "../public/notes.html"));
 });
-
+// Routes to the index.html webpage
 router.get("*", (req,res) => {
     res.sendFile(path.join(__dirname, "../public/index.html"));
 });
