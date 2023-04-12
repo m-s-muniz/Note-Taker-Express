e9dfcbc9796a3a4ab9df03fdbd01bebf6cfec105
Michael Muniz <m.s.muniz@sbcglobal.net>
2023-04-12 07:04:30 -0500

    Added GET route for retrieving all notes and a POST route for submitting the new note. Also in the previous commit a npm was added called 'uuid' to give notes a unique id.

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,7 +1,24 @@
 const router = require('express').Router();
 const { v4: uuidv4 } = require('uuid');
 const fs = require ("fs");
+// GET Route for retrieving all the notes from db.json file
+router.get("/notes", (req, res) => {
+    const data = fs.readFileSync("db/db.json");
+    res.json(JSON.parse(data));
+});
+// POST Route for submitting newNote, gives it a unique identifier and saves new note
+router.post("/notes",(req, res) => {
+    const notes = JSON.parse(fs.readFileSync("db/db.json","utf8"));
+    const newNote = {
+        title: req.body.title,
+        text: req.body.text,
+        id: uuidv4(),
+    };
+    notes.push(newNote);
+    fs.writeFileSync("db/db.json", JSON.stringify(notes));
+    res.json(notes);
 
+});
 
 
 
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -6,7 +6,7 @@ router.get("/notes", (req,res) => {
     res.sendFile(path.join(__dirname, "../public/notes.html"));
 });
 // Routes to the index.html webpage
-router.get("*", (req,res) => {
+router.get("/", (req,res) => {
     res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 
